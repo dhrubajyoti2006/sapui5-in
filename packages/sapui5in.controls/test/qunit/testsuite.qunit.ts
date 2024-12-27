83921f1a867fb140feb143e9aacc01bcac8657fb
Dhrubajyoti Rakshit <dhrubajyoti.rakshit@vistex.com>
2024-12-28 00:31:02 +0100

    Initial Commit: It creates a boilerplate sapui5 application and a control library, both in typescript

---
+++ packages/sapui5in.controls/test/qunit/testsuite.qunit.ts
@@ -0,0 +1,25 @@
+export default {
+	name: "QUnit TestSuite for sapui5in.controls",
+	defaults: {
+		ui5: {
+			libs: ["sap.ui.core", "sapui5in.controls"],
+			theme: "sap_horizon"
+		},
+		qunit: {
+			version: 2,
+			reorder: false
+		},
+		sinon: {
+			version: 4,
+			qunitBridge: true,
+			useFakeTimers: false
+		}
+	},
+	tests: {
+		// test file for the Example control
+		Example: {
+			title: "QUnit Test for Example",
+			_alternativeTitle: "QUnit tests: sapui5in.controls.Example"
+		}
+	}
+};
